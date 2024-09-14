d1692aa202c80ddffb3fe80c1082dd43c2aee8bd
shahrvin <shervinmihan@gmail.com>
2024-09-15 00:06:59 +0330

    using multiple threads

--- threading2.py
+++ threading2.py
@@ -3,20 +3,30 @@ import logging
 import time
 
 
-def thred_func(name):
+def thread_func(name):
     logging.info("thread %s: starting",name)
     time.sleep(2)
     logging.info("thread %s: finishing", name)
 
 if __name__ == "__main__":
     logging.basicConfig(format= "%(asctime)s: %(message)s", level=logging.INFO, datefmt="%H: %M: %S")
-    logging.info("main: before creating thread")
-    x  = threading.Thread(target=thred_func, args= (1,))
-    logging.info("main: before running thread")
-    x.start()
-    logging.info("main: wait for thread to finish")
-    # x.join()
-    logging.info("main: all done")
+    logging.info("main: before creating threads")
+
+    threads = list()
+    for index in range(3):
+        logging.info("main:  create and start thread %d.", index)
+
+        x  = threading.Thread(target=thread_func, args= (index,))
+        threads.append(x)
+        x.start()
+
+    for index , thread in enumerate(threads):
+
+        logging.info("main: before joining thread %d", index)
+        thread.join()
+    
+        logging.info("main: thread %d is done.", index)
+    
 
 
 
